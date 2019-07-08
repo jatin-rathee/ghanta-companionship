03cf1fa4870f4ec3d8658c878c0872bb2e6ea498
jatin-rathee <jatinrathee2018@gmail.com>
2019-07-09 00:15:20 +0530

    UI Changes

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,19 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+var start = document.querySelector(".start");
+var login = document.querySelector(".login");
+var submit_btn = document.querySelector("#submit_btn");
+var login_btn = document.querySelector("#login_btn");
+var main_page = document.querySelector(".main_page");
+
+login_btn.addEventListener('click',function() {
+  start.style.display = "none";
+  login.style.display = "block";
+});
+
+submit_btn.addEventListener("click",function() {
+  login.style.display = "none";
+  main_page.style.display = "block";
+});
